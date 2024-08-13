1bc40cd65d8e2178ebacb9199a4153d665ea84d6
Pachara004 <147172411+Pachara004@users.noreply.github.com>
2024-08-13 21:40:28 +0700

    Update register.dart

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:lotto_app/pages/login.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -8,8 +10,122 @@ class RegisterPage extends StatefulWidget {
 
 class _RegisterPageState extends State<RegisterPage> {
   bool _isAccepted = false;
-  // This variable will hold the selected profile image
   ImageProvider? _profileImage;
+
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController = TextEditingController();
+
+  Future<bool> _isUsernameTaken(String username) async {
+    final String checkUrl = 'https://nodejs-wfjd.onrender.com/signup/checkusername';
+    final response = await http.post(
+      Uri.parse(checkUrl),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'username': username,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = jsonDecode(response.body);
+      return data['exists'];
+    } else {
+      _showErrorDialog('Failed to connect to the server');
+      return false;
+    }
+  }
+
+  Future<void> _register() async {
+    if (_passwordController.text != _confirmPasswordController.text) {
+      _showErrorDialog('Passwords do not match');
+      return;
+    }
+
+    if (!_isAccepted) {
+      _showErrorDialog('You must accept the terms and conditions');
+      return;
+    }
+
+    // Check if the username is already taken
+    final isUsernameTaken = await _isUsernameTaken(_usernameController.text);
+    if (isUsernameTaken) {
+      _showErrorDialog('Username is already taken');
+      return;
+    }
+
+    final String apiUrl = 'https://nodejs-wfjd.onrender.com/signup';
+    final response = await http.post(
+      Uri.parse(apiUrl),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'username': _usernameController.text,
+        'phone': _phoneController.text,
+        'email': _emailController.text,
+        'password': _passwordController.text,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = jsonDecode(response.body);
+      if (data['success']) {
+        _showSuccessDialog('Registration Successful');
+      } else {
+        _showErrorDialog('Registration failed');
+      }
+    } else {
+      _showErrorDialog('Failed to connect to the server');
+    }
+  }
+
+  void _showSuccessDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Success'),
+          content: Text(message),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage()),
+                );
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Error'),
+          content: Text(message),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,44 +134,39 @@ class _RegisterPageState extends State<RegisterPage> {
         title: const Text(''),
         backgroundColor: const Color(0xFF453BC9),
       ),
-      body: SingleChildScrollView( // Make the content scrollable
+      body: SingleChildScrollView(
         child: Column(
           children: [
-            // Top section with background color
             Container(
-              color: const Color(0xFF453BC9), // Set the background color to #453BC9
-              height: 100, // Adjust the height as needed
-              width: double.infinity, // Make the container full width
+              color: const Color(0xFF453BC9),
+              height: 100,
+              width: double.infinity,
               child: const Center(
                 child: Text(
                   'Sign Up',
                   style: TextStyle(
                     fontWeight: FontWeight.w800,
-                    color: Colors.white, // Set text color to white
-                    fontSize: 24, // Set font size
+                    color: Colors.white,
+                    fontSize: 24,
                   ),
                 ),
               ),
             ),
-            // Remaining part of the screen
             Container(
-              color: Colors.white, // Background color for the rest of the screen
+              color: Colors.white,
               padding: const EdgeInsets.all(20.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  // Profile image section
                   Center(
                     child: GestureDetector(
                       onTap: () {
-                        // Handle image selection logic here
-                        // For now, just simulate image selection
                         setState(() {
-                          _profileImage = AssetImage('assets/default_profile.png'); // Replace with your default image
+                          _profileImage = AssetImage('assets/default_profile.png');
                         });
                       },
                       child: CircleAvatar(
-                        radius: 50.0, // Adjust the radius as needed
+                        radius: 50.0,
                         backgroundImage: _profileImage,
                         child: _profileImage == null
                             ? const Icon(Icons.camera_alt, size: 40.0, color: Colors.white)
@@ -63,64 +174,69 @@ class _RegisterPageState extends State<RegisterPage> {
                       ),
                     ),
                   ),
-                  const SizedBox(height: 16.0), // Space between profile image and username field
+                  const SizedBox(height: 16.0),
 
-                  const TextField(
-                    decoration: InputDecoration(
+                  TextField(
+                    controller: _usernameController,
+                    decoration: const InputDecoration(
                       labelText: 'Username',
                       border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(45.0)), // Rounded corners with 45-degree angle
+                        borderRadius: BorderRadius.all(Radius.circular(45.0)),
                       ),
                       prefixIcon: Icon(Icons.person),
                     ),
                   ),
-                  const SizedBox(height: 16.0), // Add space between text fields
+                  const SizedBox(height: 16.0),
 
-                  const TextField(
-                    decoration: InputDecoration(
+                  TextField(
+                    controller: _phoneController,
+                    decoration: const InputDecoration(
                       labelText: 'Phone',
                       border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(45.0)), // Rounded corners with 45-degree angle
+                        borderRadius: BorderRadius.all(Radius.circular(45.0)),
                       ),
                       prefixIcon: Icon(Icons.phone),
                     ),
                   ),
-                  const SizedBox(height: 16.0), // Add space between text fields
+                  const SizedBox(height: 16.0),
 
-                  const TextField(
-                    decoration: InputDecoration(
+                  TextField(
+                    controller: _emailController,
+                    decoration: const InputDecoration(
                       labelText: 'Email',
                       border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(45.0)), // Rounded corners with 45-degree angle
+                        borderRadius: BorderRadius.all(Radius.circular(45.0)),
                       ),
                       prefixIcon: Icon(Icons.email),
                     ),
                   ),
-                  const SizedBox(height: 16.0), // Add space between text fields
+                  const SizedBox(height: 16.0),
 
-                  const TextField(
+                  TextField(
+                    controller: _passwordController,
                     obscureText: true,
-                    decoration: InputDecoration(
+                    decoration: const InputDecoration(
                       labelText: 'Password',
                       border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(45.0)), // Rounded corners with 45-degree angle
+                        borderRadius: BorderRadius.all(Radius.circular(45.0)),
                       ),
                       prefixIcon: Icon(Icons.lock),
                     ),
                   ),
-                  const SizedBox(height: 16.0), // Add space between text fields
+                  const SizedBox(height: 16.0),
 
-                  const TextField(
+                  TextField(
+                    controller: _confirmPasswordController,
                     obscureText: true,
-                    decoration: InputDecoration(
+                    decoration: const InputDecoration(
                       labelText: 'Confirm Password',
                       border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(45.0)), // Rounded corners with 45-degree angle
+                        borderRadius: BorderRadius.all(Radius.circular(45.0)),
                       ),
                       prefixIcon: Icon(Icons.lock),
                     ),
                   ),
-                  const SizedBox(height: 16.0), // Add space between text fields
+                  const SizedBox(height: 16.0),
 
                   Row(
                     children: [
@@ -140,22 +256,19 @@ class _RegisterPageState extends State<RegisterPage> {
                       ),
                     ],
                   ),
-                  const SizedBox(height: 24.0), // Add space before the button
+                  const SizedBox(height: 24.0),
 
-                  // Center the button
                   Center(
                     child: ElevatedButton(
-                      onPressed: () {
-                        // Handle sign-up action
-                      },
+                      onPressed: _register,
                       style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFF453BC9), // Match button color
+                        backgroundColor: const Color(0xFF453BC9),
                         padding: const EdgeInsets.symmetric(
                           horizontal: 50.0,
                           vertical: 15.0,
                         ),
                         shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(45.0), // Rounded button corners
+                          borderRadius: BorderRadius.circular(45.0),
                         ),
                       ),
                       child: const Text(
@@ -168,9 +281,8 @@ class _RegisterPageState extends State<RegisterPage> {
                       ),
                     ),
                   ),
-                  const SizedBox(height: 20.0), // Space before the sign-up text
+                  const SizedBox(height: 20.0),
 
-                  // Sign up text with link
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
